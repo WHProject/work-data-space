53ce57447dd8083b485366dd724ab69f0c5cf715
chengwei2 <chengwei2@JXWBJ1207261100.jiuxian.local>
2017-08-01 11:22:40 +0800

    update

--- work/src/main/java/Test.java
+++ work/src/main/java/Test.java
@@ -1,10 +1,15 @@
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Test {
 	public static void main(String[] args) {
 		String message = "295999757@qq.com";
 		Byte s = null;
 		BigDecimal rateValue = new BigDecimal("-0.0000001");
-		System.out.println((new BigDecimal(0)).compareTo(rateValue) == 1 || (new BigDecimal(49)).compareTo(rateValue) == -1);
+		List<Integer> productIds =new ArrayList<Integer>();
+		productIds.add(123);
+		productIds.add(44);
+		System.out.println(productIds);
 	}
 }
--- work/代理商品服务费调整/代理商品服务费相关.sql
+++ work/代理商品服务费调整/代理商品服务费相关.sql
@@ -7,7 +7,7 @@ ADD COLUMN `rate`  decimal(4,4) NOT NULL DEFAULT 0.0500 COMMENT '加盟商服务
 #2.店铺商品SKU数量调整
 update tg_Shop t set t.max_insale_count='50' where t.max_insale_count='30';
 
-USE zntg_jxweb;
+USE zntg_admin;
 
-#2.代理商品服务费扣点调整新增权限
-INSERT INTO `zntg_admin`.`adm_Power` (`ID`, `ParentID`, `PowerName`, `Url`, `Sort`, `Type`, `State`, `CreateTime`, `ModifyTime`, `Ext1`) VALUES ('1299', '1275', '更新加盟商服务费比例', '/proxyManage/product/rate/update.htm', '0', '3', '1', '2017-07-14 16:24:32', NULL, NULL);
+#3.代理商品服务费扣点调整新增权限
+INSERT INTO `zntg_admin`.`adm_Power` (`ID`, `ParentID`, `PowerName`, `Url`, `Sort`, `Type`, `State`, `CreateTime`, `ModifyTime`, `Ext1`) VALUES ('1300', '1275', '更新加盟商服务费比例', '/proxyManage/product/rate/update.htm', '0', '3', '1', '2017-07-14 16:24:32', NULL, NULL);
