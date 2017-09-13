2f6a91df55091ffe770277e0232c3e09685f553d
chengwei2 <chengwei2@JXWBJ1207261100.jiuxian.local>
2017-09-13 11:31:05 +0800

    update

--- work/代理商品设置起批数量及积分兑换抽奖/代理商品设置起批数量及积分兑换抽奖.sql
+++ work/代理商品设置起批数量及积分兑换抽奖/代理商品设置起批数量及积分兑换抽奖.sql
@@ -6,7 +6,7 @@ ADD COLUMN `initial_wholesale_quantity`  int(11) DEFAULT NULL COMMENT '起批数
 
 #2.新增积分兑换抽奖次数相关字段
 ALTER TABLE `tg_LuckyDraw`
-ADD COLUMN `use_integral_sign`  bit(1) NULL COMMENT '积分抽奖:0=不选中；1=选中' AFTER `LoginSign`,
+ADD COLUMN `use_integral_sign`  bit(1) DEFAULT b'0' COMMENT '积分抽奖:0=不选中；1=选中' AFTER `LoginSign`,
 ADD COLUMN `use_integral`  decimal(10,2) NULL COMMENT '消费积分数量' AFTER `UseMoney`,
 ADD COLUMN `use_integral_count`  int(11) NULL COMMENT '消耗积分兑换抽奖次数' AFTER `UseMoneyCount`;
 
